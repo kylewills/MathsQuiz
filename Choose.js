f3218f0d689605163240d14c005955d32cdc9c11
kylewills <32134945+kylewills@users.noreply.github.com>
2018-11-15 18:15:06 +0000

    First commit

---
+++ Choose.js
@@ -0,0 +1,27 @@
+//javascript
+
+/*function goToGame() {
+    window.location.href = "https://www.example.com";
+}*/
+
+document.getElementById("box1").onclick = function() {
+    window.location.href = "/multiply.html";
+}
+
+document.getElementById("box2").onclick = function() {
+    window.location.href = "/add.html";
+}
+
+
+document.getElementById("box3").onclick = function() {
+    window.location.href = "/takeaway.html";
+}
+
+
+document.getElementById("box4a").onclick = function() {
+    window.location.href = "/dividedby2.html";
+}
+
+
+            
+
